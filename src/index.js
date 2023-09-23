27e5200c2fa7e39a0b3feffbbf84e092bddc5a63
QTNA <quan7colorful@outlook.com>
2023-09-23 21:19:34 +0700

    Run MongoDB complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('dotenv/config')
 
 const { Client, GatewayIntentBits } = require('discord.js');
+const mongoose = require('mongoose')
 const { CommandKit } = require('commandkit')
 const path = require('path')
 
@@ -16,6 +17,16 @@ new CommandKit({
     client,
     eventsPath: path.join(__dirname, 'events'),
     commandsPath: path.join(__dirname, 'commands'),
-})
+});
+
+(async () => {
+    try {
+        mongoose.set('strictQuery', false),
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('MongoDB | Status : Connected');
+    } catch (error) {
+        console.log(`Error: ${error}`);
+    }
+})();
 
 client.login(process.env.TOKEN)
